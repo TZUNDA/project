d7fc60d5d3a0ac15b2ce0c4550407fa1be7491a8
tzalez <tzalez91@gmail.com>
2015-07-16 09:03:38 +0200

    Update index.php

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ and open the template in the editor.
        <?php echo '<p>Hola Mundo y esos Aliens y el Campos de trigoooooo yeHHH trtrtr</p>'; 
        //te he comitado encima tio cata el historial juajajaja xk los estoy aciendo desde la web y aqui no se hacer pull
        //Se hacen auto en la version web me da ammi porque me ha aparecido del tiron actualizado
+       //nose yo algo raro e catao
        ?>
        
     </body>
