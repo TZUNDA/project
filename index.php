1ceb77a3c956c5c01635ce60dea7cf128aea4ff5
Alvaro <tzalez91@gmail.com>
2015-07-16 12:31:55 +0200

    Funcion de seguridad

--- index.php
+++ index.php
@@ -14,6 +14,28 @@ and open the template in the editor.
        //te he comitado encima tio cata el historial juajajaja xk los estoy aciendo desde la web y aqui no se hacer pull
        //Se hacen auto en la version web me da ammi porque me ha aparecido del tiron actualizado
        //nose yo algo raro e catao
+       
+       //esta funcion sirve para que al acceder a los datos que envia el user en un form, no se puede inyectar ni scripts ni htlm 
+       //vamos que para seguridad
+       /* ?php
+// define variables and set to empty values
+$name = $email = $gender = $comment = $website = "";
+//El formulario a sido enviado esto seria su procesado en el action
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = test_input($_POST["name"]);
+  $email = test_input($_POST["email"]);
+  $website = test_input($_POST["website"]);
+  $comment = test_input($_POST["comment"]);
+  $gender = test_input($_POST["gender"]);
+}
+
+function test_input($data) {
+  $data = trim($data);// quita espacios y tabulados etc
+  $data = stripslashes($data);// quita los backslash
+  $data = htmlspecialchars($data);// cadifica string para prevenir inyecciones
+  return $data;
+}
+?>*/
        ?>
        
     </body>
