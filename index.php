5ae6fcdf9f74fc0d41e4da72d69504ad719d0936
Alvaro <Alvaro@192.168.1.36>
2015-07-16 20:38:22 +0200

    PRUEBA TZUNDA LOCAL

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>PRUEBA LOCAL TZUNDA TZALEZ</title>
     </head>
     <body>
        <?php echo '<p>Hola Mundo y esos Aliens y el Campos de trigoooooo yeHHH trtrtr</p>'; 
