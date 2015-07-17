925b0095c8feda86c07a7386d8ac8930ac153b41
Alvaro <tzalez91@gmail.com>
2015-07-17 08:56:21 +0200

    Un form pa que veas si quieres.
    Deberia funcionar aunque lo he ejucutao en un pagina

--- index.php
+++ index.php
@@ -16,44 +16,96 @@ and open the template in the editor.
         <script src="js/nuestro.js"></script>
     </head>
     <body>
-       <?php echo '<p>Hola Mundo y esos Aliens y el Campos de trigoooooo yeHHH trtrtr</p>'; 
-       //te he comitado encima tio cata el historial juajajaja xk los estoy aciendo desde la web y aqui no se hacer pull
-       //Se hacen auto en la version web me da ammi porque me ha aparecido del tiron actualizado
-       //nose yo algo raro e catao
-       
-       //esta funcion sirve para que al acceder a los datos que envia el user en un form, no se puede inyectar ni scripts ni htlm 
-       //vamos que para seguridad
-       /* 
-       <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-       Name: <input type="text" name="name">
-        E-mail: <input type="text" name="email">
-        Website: <input type="text" name="website">
-        Comment: <textarea name="comment" rows="5" cols="40"></textarea>
-        Gender:
-        <input type="radio" name="gender" value="female">Female
-        <input type="radio" name="gender" value="male">Male
-        </form>
-        
+       <?php echo '<p>Hola Mundo y esos Aliens y el Campos de trigoooooo yeHHH trtrtr</p>';?>
        <?php
 // define variables and set to empty values
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
 $name = $email = $gender = $comment = $website = "";
-//El formulario a sido enviado esto seria su procesado en el action
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = test_input($_POST["name"]);
-  $email = test_input($_POST["email"]);
-  $website = test_input($_POST["website"]);
-  $comment = test_input($_POST["comment"]);
-  $gender = test_input($_POST["gender"]);
+	$ok=0;
+   if (empty($_POST["name"])) {
+     $nameErr = "Name is required";
+	   $ok++;
+   } else {
+     $name = test_input($_POST["name"]);
+     // check if name only contains letters and whitespace
+     if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+       $nameErr = "Only letters and white space allowed";
+		 $ok++;
+     }
+   }
+  
+   if (empty($_POST["email"])) {
+     $emailErr = "Email is required";
+	    $ok++;
+   } else {
+     $email = test_input($_POST["email"]);
+     // check if e-mail address is well-formed
+     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+       $emailErr = "Invalid email format";
+		 $ok++;
+     }
+   }
+    
+   if (empty($_POST["website"])) {
+     $website = "";
+   } else {
+     $website = test_input($_POST["website"]);
+     // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
+     if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
+       $websiteErr = "Invalid URL";
+		 $ok++;
+     }
+   }
+
+   if (empty($_POST["comment"])) {
+     $comment = "";
+   } else {
+     $comment = test_input($_POST["comment"]);
+   }
+
+   if (empty($_POST["gender"])) {
+     $genderErr = "Gender is required";
+	   $ok++;
+   } else {
+     $gender = test_input($_POST["gender"]);
+   }
+	if($ok==0){
+	header("Location: http://www.google.es");
+	exit();
+	}
 }
 
 function test_input($data) {
-  $data = trim($data);// quita espacios y tabulados etc
-  $data = stripslashes($data);// quita los backslash
-  $data = htmlspecialchars($data);// cadifica string para prevenir inyecciones
-  return $data;
+   $data = trim($data);
+   $data = stripslashes($data);
+   $data = htmlspecialchars($data);
+   return $data;
 }
-?>*/
-       ?>
+?>
+
+<h2>PHP Form Validation Example</h2>
+<p><span class="error">* required field.</span></p>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+   Name: <input type="text" name="name" value="<?php echo $name;?>">
+   <span class="error">* <?php echo $nameErr;?></span>
+   <br><br>
+   E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+   <span class="error">* <?php echo $emailErr;?></span>
+   <br><br>
+   Website: <input type="text" name="website" value="<?php echo $website;?>">
+   <span class="error"><?php echo $websiteErr;?></span>
+   <br><br>
+   Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+   <br><br>
+   Gender:
+   <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?>  value="female">Female
+   <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?>  value="male">Male
+   <span class="error">* <?php echo $genderErr;?></span>
+   <br><br>
+   <input type="submit" name="submit" value="Submit">
+</form>
        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
        <script src="js/bootstrap.min.js"></script>
     </body>
