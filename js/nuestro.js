0c294278ca8fe2707c2d2553e4f631b33a52221c
Alvaro <tzalez91@gmail.com>
2015-07-20 13:09:32 +0200

    Active class

--- js/nuestro.js
+++ js/nuestro.js
@@ -3,5 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+$(".nav li").on("click", function() {
+      $(".nav li").removeClass("active");
+      $(this).addClass("active");
+    });
 
