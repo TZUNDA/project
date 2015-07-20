8d1742c3dab1c303615d54b32e65074ebcf8fa75
bunda60 <>
2015-07-20 18:13:01 +0200

    msg 00

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ and open the template in the editor.
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-         <title>PRUEBA LOCAL TZUNDA TZALEZ</title>
+         <title>PRUEBA LOCAL</title>
         <link href="css/bootstrap.css" rel="stylesheet">
         <link href="css/estilos.css" rel="stylesheet">
        <!-- <script src="js/modernizr-2.8.3-respond-1.4.2.min.js"></script>//Esto ya veremos-->
