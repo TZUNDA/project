6c6edb73d6ec596e4d2c25e360bd3da3fa8aff38
Alvaro <tzalez91@gmail.com>
2015-07-21 10:02:08 +0200

    Update nuestro.js

--- js/nuestro.js
+++ js/nuestro.js
@@ -13,6 +13,36 @@ $(".nav li").on("click", function() {
 $(".navbar-brand").on("click", function() {
       $(".nav li").removeClass("active");
     });
+    
+/* activate sidebar */
+$('#sidebar').affix({
+  offset: {
+    top: 235
+  }
+});
+
+/* activate scrollspy menu */
+var $body   = $(document.body);
+var navHeight = $('.navbar').outerHeight(true) + 10;
+
+$body.scrollspy({
+	target: '#leftCol',
+	offset: navHeight
+});
+
+/* smooth scrolling sections */
+$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - 50
+        }, 1000);
+        return false;
+      }
+    }
+});
 };
 
 
