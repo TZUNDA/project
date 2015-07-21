fe526591168bf61947529ef02d43da256080766e
Alvaro <tzalez91@gmail.com>
2015-07-21 13:42:33 +0200

    Update rsBuscar.php

--- rsBuscar.php
+++ rsBuscar.php
@@ -13,5 +13,53 @@ and open the template in the editor.
         <?php
         // put your code here
         ?>
+        
+        <div class="container">
+  <div class="row">
+    <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
+   
+      <h2>16:9 Responsive Aspect Ratio</h2>
+<div class="embed-responsive embed-responsive-16by9">
+    <iframe class="embed-responsive-item" src="//www.youtube.com/embed/ePbKGoIGAXY"></iframe>
+</div>
+    </div>
+    <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
+   
+      <h2>16:9 Responsive Aspect Ratio</h2>
+<div class="embed-responsive embed-responsive-16by9">
+    <iframe class="embed-responsive-item" src="//www.youtube.com/embed/ePbKGoIGAXY"></iframe>
+</div>
+    </div>
+    <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
+   
+      <h2>16:9 Responsive Aspect Ratio</h2>
+<div class="embed-responsive embed-responsive-16by9">
+    <iframe class="embed-responsive-item" src="//www.youtube.com/embed/ePbKGoIGAXY"></iframe>
+</div>
+    </div>
+    <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
+   
+      <h2>16:9 Responsive Aspect Ratio</h2>
+<div class="embed-responsive embed-responsive-16by9">
+    <iframe class="embed-responsive-item" src="//www.youtube.com/embed/ePbKGoIGAXY"></iframe>
+</div>
+    </div>
+    <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
+   
+      <h2>16:9 Responsive Aspect Ratio</h2>
+<div class="embed-responsive embed-responsive-16by9">
+    <iframe class="embed-responsive-item" src="//www.youtube.com/embed/ePbKGoIGAXY"></iframe>
+</div>
+    </div>
+    <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
+ 
+
+      <h2>4:3 Responsive Aspect Ratio</h2>
+<div class="embed-responsive embed-responsive-4by3">
+    <iframe class="embed-responsive-item" src="//www.youtube.com/embed/ePbKGoIGAXY"></iframe>
+</div>
+    </div>
+  </div>
+</div>
     </body>
 </html>
